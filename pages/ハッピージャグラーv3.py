52525bfeb2a8c58fac26bd6a5c922e47445bc5df
showShimada <sho.shimada.1131@gmail.com>
2025-01-15 17:29:00 +0900

    総合判定含めて完成

--- pages/ハッピージャグラーv3.py
+++ pages/ハッピージャグラーv3.py
@@ -47,16 +47,6 @@ df_probabilities_for_display = pd.DataFrame({
 df_probabilities_for_display = df_probabilities_for_display.set_index("設定")
 st.dataframe(df_probabilities_for_display)
 
-# df_probabilities_detail = pd.DataFrame({
-#     "BBバラケ目":[(single_flag + rare_flag + floor(rare_cherry_flag/2))/amount_flag_count for single_flag, rare_flag, rare_cherry_flag in zip(BB_single_flag_counts,BB_rare_flag_counts,BB_rare_cherry_flag_counts)],
-#     "BBチェリー重複":[(cherry_flag + ceil(rare_cherry_flag/2))/amount_flag_count for cherry_flag, rare_cherry_flag in zip(BB_cherry_flag_counts,BB_rare_cherry_flag_counts)],
-#     "RB単独":[flag_count/amount_flag_count for flag_count in RB_single_flag_counts],
-#     "RBチェリー重複":[flag_count/amount_flag_count for flag_count in RB_cherry_flag_counts],
-#     "ブドウ":[flag_count/amount_flag_count for flag_count in grapes_flag_counts],
-#     "単独チェリー":[flag_count/amount_flag_count for flag_count in single_cherry_flag_counts],
-#     "ハズレ":[(amount_flag_count - BB_flag_count - RB_flag_count - grapes_flag_count - single_cherry_flag_count)/amount_flag_count for BB_flag_count,RB_flag_count,grapes_flag_count,single_cherry_flag_count in zip(BB_flag_counts,RB_flag_counts,grapes_flag_counts,single_cherry_flag_counts)]
-# })
-
 option = st.selectbox(
     "入力内容を選んで下さい",
     ("ボーナス確率のみ","ボーナス確率とブドウ","詳細版")
@@ -122,13 +112,7 @@ elif option == "ボーナス確率とブドウ":
         df_probabilities = pd.DataFrame({
             "BB":[row.BB_flag_counts/row.amount_flag_counts for row in df_flag_counts.itertuples(index=False, name="setting")],
             "RB":[row.RB_flag_counts/row.amount_flag_counts for row in df_flag_counts.itertuples(index=False, name="setting")],
-            "ハズレ":[(row.amount_flag_counts - row.BB_flag_counts - row.RB_flag_counts)/row.amount_flag_counts for row in df_flag_counts.itertuples(index=False, name="setting")]
-        })
-        df_probabilities_include_grapes = pd.DataFrame({
-            "BB":[row.BB_flag_counts/row.amount_flag_counts for row in df_flag_counts.itertuples(index=False, name="setting")],
-            "RB":[row.RB_flag_counts/row.amount_flag_counts for row in df_flag_counts.itertuples(index=False, name="setting")],
-            "ブドウ":[row.grapes_flag_counts/row.amount_flag_counts for row in df_flag_counts.itertuples(index=False, name="setting")],
-            "ハズレ":[(row.amount_flag_counts - row.BB_flag_counts - row.RB_flag_counts - row.grapes_flag_counts)/row.amount_flag_counts for row in df_flag_counts.itertuples(index=False, name="setting")]
+            "ブドウ":[row.grapes_flag_counts/row.amount_flag_counts for row in df_flag_counts.itertuples(index=False, name="setting")]
         })
 
         """
@@ -142,24 +126,122 @@ elif option == "ボーナス確率とブドウ":
         },index=["結果"])
         st.dataframe(df_outcome)
 
-        plt = modules.create_pie_graph_only_bonuses(BB_count,RB_count,game_count,df_probabilities)
-        # plt = modules.create_pie_graph_include_grapes(BB_count,RB_count,grapes_count,game_count,df_probabilities_include_grapes)
+        plt = modules.create_pie_graph_include_grapes(BB_count,RB_count,grapes_count,game_count,df_flag_counts)
         st.pyplot(plt)
 
         """
         # BBの二項分布
         """
-        plt = modules.create_binom_graph("BB", BB_count, game_count, df_probabilities_include_grapes,5)
+        plt = modules.create_binom_graph("BB", BB_count, game_count, df_probabilities,5)
         st.pyplot(plt)
 
         """
         # RBの二項分布
         """
-        plt = modules.create_binom_graph("RB", RB_count, game_count, df_probabilities_include_grapes,5)
+        plt = modules.create_binom_graph("RB", RB_count, game_count, df_probabilities,5)
         st.pyplot(plt)
 
         """
         # ブドウの二項分布
         """
-        plt = modules.create_binom_graph("ブドウ", grapes_count, game_count, df_probabilities_include_grapes,20)
+        plt = modules.create_binom_graph("ブドウ", grapes_count, game_count, df_probabilities,20)
+        st.pyplot(plt)
+
+elif option == "詳細版":
+    """
+    # 入力欄
+    """
+    dict_inputs ={
+        "BB_single_count":st.number_input("BBバラケ目回数",0)
+        ,"BB_cherry_count":st.number_input("BBチェリー重複回数",0)
+        ,"RB_single_count":st.number_input("RB単独回数",0)
+        ,"RB_cherry_count":st.number_input("RBチェリー重複回数",0)
+        ,"grapes_count":st.number_input("ブドウ",0)
+        ,"cherry_count":st.number_input("ペカらないチェリー",0)
+        ,"START_count":st.number_input("開始ゲーム数",0)
+        ,"END_count":st.number_input("現在ゲーム数",3000)
+    }
+    game_count = dict_inputs["END_count"] - dict_inputs["START_count"]
+    cherry_sum = dict_inputs["BB_cherry_count"] + dict_inputs["RB_cherry_count"] + dict_inputs["cherry_count"]
+
+    # 判別パート
+    if st.button("実行"):
+
+        df_probabilities = pd.DataFrame({
+            "BBバラケ目":[(row.BB_single_flag_counts + row.BB_rare_flag_counts + floor(row.BB_rare_cherry_flag_counts/2))/row.amount_flag_counts
+                        for row in df_flag_counts.itertuples(index=False, name="setting")],
+            "BBチェリー重複":[(row.BB_cherry_flag_counts + ceil(row.BB_rare_cherry_flag_counts/2))/row.amount_flag_counts
+                        for row in df_flag_counts.itertuples(index=False, name="setting")],
+            "RB単独":[row.RB_single_flag_counts/row.amount_flag_counts
+                        for row in df_flag_counts.itertuples(index=False, name="setting")],
+            "RBチェリー重複":[row.RB_cherry_flag_counts/row.amount_flag_counts
+                        for row in df_flag_counts.itertuples(index=False, name="setting")],
+            "ブドウ":[row.grapes_flag_counts/row.amount_flag_counts
+                        for row in df_flag_counts.itertuples(index=False, name="setting")],
+            "ペカらないチェリー":[row.single_cherry_flag_counts/row.amount_flag_counts
+                        for row in df_flag_counts.itertuples(index=False, name="setting")],
+            "チェリー重複率":[(row.BB_cherry_flag_counts + ceil(row.BB_rare_cherry_flag_counts/2) + row.RB_cherry_flag_counts)/(row.single_cherry_flag_counts + row.BB_cherry_flag_counts + ceil(row.BB_rare_cherry_flag_counts/2) + row.RB_cherry_flag_counts)
+                        for row in df_flag_counts.itertuples(index=False, name="setting")]
+        })
+
+        """
+        # 総合判定
+        """
+        df_outcome = pd.DataFrame({
+            "ゲーム数":game_count,
+            "BB合成":modules.get_outcome(game_count, dict_inputs["BB_single_count"] + dict_inputs["BB_cherry_count"]),
+            "RB合成":modules.get_outcome(game_count, dict_inputs["RB_single_count"] + dict_inputs["RB_cherry_count"]),
+            "BBバラケ目":modules.get_outcome(game_count, dict_inputs["BB_single_count"]),
+            "BBチェリー重複":modules.get_outcome(game_count, dict_inputs["BB_cherry_count"]),
+            "RB単独":modules.get_outcome(game_count, dict_inputs["RB_single_count"]),
+            "RBチェリー重複":modules.get_outcome(game_count, dict_inputs["RB_cherry_count"]),
+            "チェリー重複率(%)":round(((dict_inputs["BB_cherry_count"] + dict_inputs["RB_cherry_count"])/cherry_sum)*100,1),
+            "ブドウ":modules.get_outcome(game_count, dict_inputs["grapes_count"]),
+            "ペカらないチェリー":modules.get_outcome(game_count, dict_inputs["cherry_count"])
+        },index=["結果"])
+        st.dataframe(df_outcome)
+
+        plt = modules.create_pie_graph_full(dict_inputs,game_count,cherry_sum,df_flag_counts)
+        st.pyplot(plt)
+
+        """
+        # BBバラケ目の二項分布
+        """
+        plt = modules.create_binom_graph("BBバラケ目", dict_inputs["BB_single_count"], game_count, df_probabilities,5)
+        st.pyplot(plt)
+
+        """
+        # BBチェリー重複の二項分布
+        """
+        plt = modules.create_binom_graph("BBチェリー重複", dict_inputs["BB_cherry_count"], game_count, df_probabilities,5)
+        st.pyplot(plt)
+
+        """
+        # RB単独の二項分布
+        """
+        plt = modules.create_binom_graph("RB単独", dict_inputs["RB_single_count"], game_count, df_probabilities,5)
+        st.pyplot(plt)
+
+        """
+        # RBチェリー重複の二項分布
+        """
+        plt = modules.create_binom_graph("RBチェリー重複", dict_inputs["RB_cherry_count"], game_count, df_probabilities,5)
+        st.pyplot(plt)
+
+        """
+        # チェリー重複率の二項分布
+        """
+        plt = modules.create_binom_graph("チェリー重複率", dict_inputs["BB_cherry_count"] + dict_inputs["RB_cherry_count"], cherry_sum, df_probabilities,5)
+        st.pyplot(plt)
+
+        """
+        # ブドウの二項分布
+        """
+        plt = modules.create_binom_graph("ブドウ", dict_inputs["grapes_count"], game_count, df_probabilities,20)
+        st.pyplot(plt)
+
+        """
+        # ペカらないチェリーの二項分布
+        """
+        plt = modules.create_binom_graph("ペカらないチェリー", dict_inputs["cherry_count"], game_count, df_probabilities,10)
         st.pyplot(plt)
